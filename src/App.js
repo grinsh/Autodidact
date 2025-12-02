ea4c0065395409c389e520d4be07b49bc9958f86
grinsh <shoshigrin18@gmail.com>
2025-12-02 12:22:19 +0200

    build client connnect to port 3000 server

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import {
 
 // 📦 API Service
 // const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
-const API_URL = "http://localhost:8080";
+const API_URL = "http://localhost:3000";
 
 const apiService = {
   getUsers: async () => {
